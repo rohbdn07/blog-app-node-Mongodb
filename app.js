ffa16f70a967e620f32d455643e7dc136dc66732
rohbdn07 <rohbdn07@users.noreply.github.com>
2020-09-19 18:01:56 +0300

    displayed the blog post

--- app.js
+++ app.js
@@ -8,19 +8,42 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index", { title: "Home" });
+  const blogs = [
+    {
+      title: "computer science is evolving",
+      description: "loream ais the party where wqe sfjhihfb oeehgiohg ouh",
+    },
+    {
+      title: "programming is evolving",
+      description: "loream ais the party where wqe sfjhihfb oeehgiohg ouh",
+    },
+    {
+      title: "javascript is love",
+      description: "loream ais the party where wqe sfjhihfb oeehgiohg ouh",
+    },
+  ];
+  res.render("index", {
+    title: "Home",
+    blogs: blogs,
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", { title: "About" });
+  res.render("about", {
+    title: "About",
+  });
 });
 
 app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create" });
+  res.render("create", {
+    title: "Create",
+  });
 });
 
 app.use((req, res) => {
-  res.status(404).render("404", { title: "Error" });
+  res.status(404).render("404", {
+    title: "Error",
+  });
 });
 
 //listen
