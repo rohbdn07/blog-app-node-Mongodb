9aaf5093e31071b2fa637ea96e729b6d795c2e16
rohbdn07 <rohbdn07@users.noreply.github.com>
2020-09-20 00:32:40 +0300

    use of middleware

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+
+app.use((req, res, next) => {
+  console.log('new request is made')
+  console.log('host:', req.hostname)
+  console.log('path:', req.path)
+  console.log('method:', req.method)
+  next()
+})
 app.get("/", (req, res) => {
   const blogs = [{
       title: "computer science is evolving",
